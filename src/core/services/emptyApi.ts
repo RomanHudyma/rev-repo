226644fc43bf77eb82fe6d1b15a37ae6d9664e2c
Roman Hudyma <roman.hudymaa@gmail.com>
2023-01-27 13:06:11 +0200

    feat: add baseUrl and auth token to headers

--- src/core/services/emptyApi.ts
+++ src/core/services/emptyApi.ts
@@ -1,7 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+import { AppState } from '@store/index';
+
 const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://linkup-academy.herokuapp.com/',
+    prepareHeaders: (headers, { getState }) => {
+      const { token } = (getState() as AppState).auth;
+
+      if (token) {
+        headers.set('authentication', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
   endpoints: () => ({}),
 });
 
